c191b6c6443ab2709a2fb345f7894f5da0487062
Marcelo Zilio <marcelo.zilio@zenvia.com>
2023-10-07 18:13:46 -0300

    function to get waypoints

--- src/app/services/rotas/rotas.service.ts
+++ src/app/services/rotas/rotas.service.ts
@@ -36,4 +36,16 @@ export class RotasService {
     const url = `${this.apiUrl}/pedidos/${id}`;
     return this.http.put<Pedido>(url, pedidoAtualizado);
   }
+
+  getPercurso(): string[] {
+    const percurso: string[] = [
+      "Av. Túlio de Rose, s/n - Jardim Europa, Porto Alegre - RS",
+      "Av. João Pessoa - Farroupilha, Porto Alegre - RS",
+      "R. Comendador Caminha, s/n - Moinhos de Vento, Porto Alegre - RS",
+      "Av. Borges de Medeiros, 2035 - Menino Deus, Porto Alegre - RS"
+    ];
+  
+    return percurso;
+  }
+
 }
